a20bdcfa36188a06d2bf17496fc3d5cc62448b0e
Pedroso-hub <66489432+Pedroso-hub@users.noreply.github.com>
2024-07-31 17:55:47 -0300

    fiz alterações na classe main pra testar algumas como funcionam as variaveis de referência para objetos

--- TesteClasses.java
+++ TesteClasses.java
@@ -2,8 +2,19 @@ public class TesteClasses{
 	public static void main (String args[]){
 		Player p1 = new Player(10, "pedro", 10);
 		Player p2 = p1;
+		Player p3 = p2;
+		p2 = new Player(20, "marcia", 20);
 		p2.setName("rafael");
-		System.out.println(p1.getName());
+		p3 = p2;
+		System.out.println(p3.getName());
+		
+		
+		
+		
+		
+		
 	}
-
+	
+	
+	
 }
